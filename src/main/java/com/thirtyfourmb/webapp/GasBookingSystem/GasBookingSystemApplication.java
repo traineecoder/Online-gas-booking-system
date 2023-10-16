7f0416cedab7de7ad394f669cf37f33a49ad7fcb
chapathi <srikanth.emani123.jr@gmail.com>
2023-10-17 01:28:56 +0530

    First commit

---
+++ src/main/java/com/thirtyfourmb/webapp/GasBookingSystem/GasBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.thirtyfourmb.webapp.GasBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thirtyfourmb/webapp/GasBookingSystem/GasBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirtyfourmb.webapp.GasBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
